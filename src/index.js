dabcef48d03b52b5776750a23f98b60f3bbac8cb
jamiew555 <89786834+jamiew555@users.noreply.github.com>
2021-10-30 22:43:26 +0100

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <div className="index">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </div>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
